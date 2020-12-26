ccd4aaa14444f1eb09557ce8c1df9386429c59a6
jsjphone8 <jsjphone8@gmail.com>
2020-12-26 20:31:00 +0900

    화면전환 - Exit 아이콘을 사용해서 Unwind Segue 구현 첫 VC에 UIStoryboardSegue를 인자로 받는 @IBAction 메소드를 구현해두면 화면전환된 VC에서 버튼을 Exit 아이콘에 연결할 때 앱 내부에 정의된 모든 메소드를 스캔하여 이중 UIStoryboard 타입의 인자값을 입력받는 액션 메소드를 모두 수집해서 보여줌. Unwind 메소드라고 하는데, 이 메소드는 우리가 화면을 종료했을 때 어떤 화면으로 이동할지 여부를 알기 위한 이정표 역할을 한다.

---
+++ practice/Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 정성훈 on 2020/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
