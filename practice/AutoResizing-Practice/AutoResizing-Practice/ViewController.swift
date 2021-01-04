9582e4f73a881fdf9ca39ff9ca76b5bd4e43a1e0
jsjphone8 <jsjphone8@gmail.com>
2021-01-04 17:54:14 +0900

    오토 레이아웃 적용해보기

---
+++ practice/AutoResizing-Practice/AutoResizing-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoResizing-Practice
+//
+//  Created by 정성훈 on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
