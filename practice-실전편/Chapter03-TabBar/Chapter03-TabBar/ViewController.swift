f400f2614a5f5938cb4e817e29dbfff3c0858a3d
jsjphone8 <jsjphone8@gmail.com>
2021-01-11 18:12:16 +0900

    탭 바 커스텀 - 애니메이션 적용

--- practice-실전편/Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
+++ practice-실전편/Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
@@ -25,6 +25,18 @@ class ViewController: UIViewController {
         
         self.view.addSubview(title)
     }
+    
+    // 탭 바 토글 처리
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let tabBar = self.tabBarController?.tabBar
+//        tabBar?.isHidden = (tabBar?.isHidden == true) ? false : true
+        
+        UIView.animate(withDuration: TimeInterval(0.15)) {
+            // alpha 값이 0이면 1로, 1이면 0으로 바꾸어 준다.
+            // 호출될 때마다 점점 투명해졌다가 점점 진해질 것이다
+            tabBar?.alpha = (tabBar?.alpha == 0) ? 1 : 0
+        }
+    }
 
 
 }
