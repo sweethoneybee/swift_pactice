556159eb5feed31440f99266f11225fcb411a366
jsjphone8 <jsjphone8@gmail.com>
2021-01-15 11:46:46 +0900

    알림창 커스터마이징 알림창에 contentViewController를 삽입할 수 있음

---
+++ practice-실전편/Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+import MapKit
+
+class MapAlertViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let alertBtn = UIButton(type: .system)
+        
+        alertBtn.frame = CGRect(x: 0, y: 150, width: 100, height: 30)
+        alertBtn.center.x = self.view.frame.width / 2
+        alertBtn.setTitle("Map Alert", for: .normal)
+        alertBtn.addTarget(self, action: #selector(mapAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(alertBtn)
+    }
+    
+    @objc func mapAlert(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "여기가 맞습니까?", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+
+        let contentVC = MapKitViewController()
+        
+        // 뷰 컨트롤러를 알림창의 콘텐츠 뷰 컨트롤러 속성에 등록한다.
+        alert.setValue(contentVC, forKey: "contentViewController")
+        
+        self.present(alert, animated: true)
+    }
+}
---
+++ practice-실전편/Chapter03-Alert/Chapter03-Alert/MapKitViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+import MapKit
+
+class MapKitViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 뷰 컨트롤러에 맵킷 뷰를 추가한다.
+        let mapkitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.view = mapkitView
+        self.preferredContentSize.height = 200
+        
+        // 위치정보를 설정. 위/경도 사용.
+        let pos = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
+        
+        // 지도에서 보여줄 넓이. 일종의 축척. 숫자가 작을수록 좁은 범위를 확대시켜서 보여준다.
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        
+        // 지도 영역을 정의
+        let region = MKCoordinateRegion(center: pos, span: span)
+        
+        // 지도 뷰에 표시
+        mapkitView.region = region
+        mapkitView.regionThatFits(region)
+        
+        // 위치를 핀으로 표시
+        let point = MKPointAnnotation()
+        point.coordinate = pos
+        mapkitView.addAnnotation(point)
+    }
+}
