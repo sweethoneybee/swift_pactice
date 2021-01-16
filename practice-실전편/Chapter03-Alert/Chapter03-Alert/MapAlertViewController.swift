2d4977a048a3452600291f883f95f0d1d0483208
jsjphone8 <jsjphone8@gmail.com>
2021-01-16 22:49:27 +0900

    알림창에 이미지 넣기

--- practice-실전편/Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
+++ practice-실전편/Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -13,6 +13,15 @@ class MapAlertViewController: UIViewController {
         alertBtn.addTarget(self, action: #selector(mapAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(alertBtn)
+        
+        let imageBtn = UIButton(type: .system)
+        
+        imageBtn.frame = CGRect(x: 0, y: 200, width: 100, height: 30)
+        imageBtn.center.x = self.view.frame.width / 2
+        imageBtn.setTitle("Image Alert", for: .normal)
+        imageBtn.addTarget(self, action: #selector(imageAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(imageBtn)
     }
     
     @objc func mapAlert(_ sender: Any) {
@@ -27,6 +36,18 @@ class MapAlertViewController: UIViewController {
         let contentVC = MapKitViewController()
         
         // 뷰 컨트롤러를 알림창의 콘텐츠 뷰 컨트롤러 속성에 등록한다.
+        alert.setValue(contentVC, forKey: "contentViewController")
+        
+        self.present(alert, animated: true)
+    }
+    
+    @objc func imageAlert(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "이번 글의 평점은 다음과 같습니다", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        let contentVC = ImageViewController()
         alert.setValue(contentVC, forKey: "contentViewController")
         
         self.present(alert, animated: true)
