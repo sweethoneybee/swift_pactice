49b1f4fcc16837fed6e74a497dee63cf64241408
jsjphone8 <jsjphone8@gmail.com>
2021-01-25 23:09:17 +0900

    UIButton 커스텀 클래스 코드 작성 - 열거형, 옵저버로 스타일 관리

--- practice-실전편/Chapter03-CSButton/Chapter03-CSButton/CSButton.swift
+++ practice-실전편/Chapter03-CSButton/Chapter03-CSButton/CSButton.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+public enum CSButtonType {
+    case rect
+    case circle
+}
+
 class CSButton: UIButton {
     // 스토리보드 방식으로 커스텀
     required init(coder aDecoder: NSCoder) {
@@ -24,5 +29,47 @@ class CSButton: UIButton {
     
     init() {
         super.init(frame: CGRect.zero)
+    }
+    
+    convenience init(type: CSButtonType) {
+        self.init()
+        
+        switch type {
+        
+        case .rect :
+            self.backgroundColor = .black
+            self.layer.borderColor = UIColor.black.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 0 // 모서리는 전혀 둥글지 않게
+            self.setTitleColor(.white, for: .normal) // 글씨는 흰색으로
+            self.setTitle("Rect Button", for: .normal) // 기본문구설정
+        case .circle :
+            self.backgroundColor = .red
+            self.layer.borderColor = UIColor.blue.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 50 // 50만큼 둥글게 처리
+            self.setTitle("Rect Button", for: .normal) // 기본문구설정
+        }
+    }
+    
+    var style: CSButtonType = .rect {
+        didSet {
+            switch style {
+            
+            case .rect :
+                self.backgroundColor = .black
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0 // 모서리는 전혀 둥글지 않게
+                self.setTitleColor(.white, for: .normal) // 글씨는 흰색으로
+                self.setTitle("Rect Button", for: .normal) // 기본문구설정
+            case .circle :
+                self.backgroundColor = .red
+                self.layer.borderColor = UIColor.blue.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 50 // 50만큼 둥글게 처리
+                self.setTitle("Rect Button", for: .normal) // 기본문구설정
+            }
+        }
     }
 }
--- practice-실전편/Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
+++ practice-실전편/Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         let csBtn = CSButton()
         csBtn.frame = CGRect(x: 30, y: 50, width: 150, height: 30)
         self.view.addSubview(csBtn)
+        
+        // 인자값에 따라 다른 스타일로 결정되는 버튼 1
+        let rectBtn = CSButton(type: .rect)
+        rectBtn.frame = CGRect(x: 30, y: 200, width: 150, height: 30)
+        self.view.addSubview(rectBtn)
+        
+        // 인자값에 따라 다른 스타일로 결정되는 버튼 2
+        let circleBtn = CSButton(type: .circle)
+        circleBtn.frame = CGRect(x: 200, y: 200, width: 150, height: 30)
+        self.view.addSubview(circleBtn)
+        
+        circleBtn.style = .rect
     }
 
 
