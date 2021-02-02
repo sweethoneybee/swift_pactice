452ec2676306db5237bf025cc79a3163d241cc5b
jsjphone8 <jsjphone8@gmail.com>
2021-02-02 22:39:44 +0900

    PickerView, TextField 연계

---
+++ practice-실전편/Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
@@ -0,0 +1,143 @@
+import UIKit
+
+class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var married: UISwitch!
+    var accountlist = [String]()
+    
+    override func viewDidLoad() {
+        let picker = UIPickerView()
+        
+        // 피커뷰의 델리게이트 객체 지정
+        picker.delegate = self
+        
+        // account 텍스트 필드 입력 방식을 가상 키보드 대신 피커 뷰로 설정
+        self.account.inputView = picker
+        
+        // 툴바 객체 정의
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: 0, height: 35)
+        toolbar.barTintColor = .lightGray
+        
+        // 액세서리 뷰 영역에 툴 바를 표시
+        self.account.inputAccessoryView = toolbar
+        
+        // 툴바에 들어갈 닫기 버튼
+        let done = UIBarButtonItem()
+        done.title = "Done"
+        done.target = self
+        done.action = #selector(pickerDone)
+        
+        // 가변 폭 버튼 정의
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        // 신규 계정 등록 버튼
+        let new = UIBarButtonItem()
+        new.title = "New"
+        new.target = self
+        new.action = #selector(newAccount)
+        
+        // 버튼을 툴 바에 추가
+        toolbar.setItems([new, flexSpace, done], animated: true)
+        
+        // 기본 저장소 객체 불러오기
+        let plist = UserDefaults.standard
+        
+        // 불러온 값을 설정
+        self.name.text = plist.string(forKey: "name")
+        self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
+        self.married.isOn = plist.bool(forKey: "gender")
+    }
+    
+    // 생성할 컴포넌트의 개수를 정의
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // 지정된 컴포넌트가 가질 목록의 길이를 정의
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.accountlist.count
+    }
+    
+    // 지정된 컴포넌트의 목록 각 행에 출력될 내용을 정의
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.accountlist[row]
+    }
+    
+    // 지정된 컴포넌트의 목록 각 행을 사용자가 선택했을 때 실행할 액션을 정의
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 선택된 계정값을 텍스트 필드에 입력
+        let account = self.accountlist[row] // 선택된 계정
+        self.account.text = account
+    }
+    
+    @objc func pickerDone(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    @objc func newAccount(_ sender: Any) {
+        self.view.endEditing(true) // 일단 열려있는 입력용 뷰부터 닫아준다
+        
+        let alert = UIAlertController(title: "새 계정을 입력하세요", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField() {
+            $0.placeholder = "ex) abc@gmail.com"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default){(_) in
+            if let account = alert.textFields?[0].text {
+                // 계정 목록에 추가한다
+                self.accountlist.append(account)
+                // 계정 텍스트 필드에 표시한다
+                self.account.text = account
+                
+                
+                // 컨트롤 값을 모두 초기화한다
+                self.name.text = ""
+                self.gender.selectedSegmentIndex = 0
+                self.married.isOn = false
+            }
+        })
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func changeGender(_ sender: UISegmentedControl) {
+        let value = sender.selectedSegmentIndex
+        
+        let plist = UserDefaults.standard
+        plist.set(value, forKey: "gender")
+    }
+    
+    @IBAction func changeMarried(_ sender: UISwitch) {
+        let value = sender.isOn
+        
+        let plist = UserDefaults.standard
+        plist.set(value,forKey: "married")
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: .alert)
+            
+            alert.addTextField(){
+                $0.text = self.name.text
+            }
+        
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default){(_) in
+                let value = alert.textFields?[0].text
+                
+                let plist = UserDefaults.standard
+                plist.set(value, forKey: "name")
+                
+                self.name.text = value
+            })
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
