2230e958ba49a39c88f2e175d38ecc9609fcc25e
jsjphone8 <jsjphone8@gmail.com>
2021-04-05 23:07:30 +0900

    Intrinsic Content Size 실습

--- NetworkPractice/NetworkPractice/ArticleListTableViewController.swift
+++ NetworkPractice/NetworkPractice/ArticleListTableViewController.swift
@@ -71,7 +71,7 @@ class ArticleListTableViewController: UITableViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
---
+++ NetworkPractice/NetworkPractice/MyView.swift
@@ -0,0 +1,16 @@
+//
+//  MyView.swift
+//  NetworkPractice
+//
+//  Created by 정성훈 on 2021/04/05.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+}
