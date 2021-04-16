d16950efde6802a1d689e4f61ec968e45e55b56a
jsjphone8 <jsjphone8@gmail.com>
2021-04-16 23:47:35 +0900

    챌린지 - 로그인뷰

---
+++ AutoLayout_LoginView/AutoLayout_LoginView/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AutoLayout_LoginView
+//
+//  Created by 정성훈 on 2021/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(adjustButtonDynamicType),
+                                               name: UIContentSizeCategory.didChangeNotification,
+                                               object: nil)
+    }
+    
+    @objc func adjustButtonDynamicType() {
+        self.buttons.forEach { (button) in
+            button.titleLabel?.adjustsFontForContentSizeCategory = true;
+        }
+    }
+}
+
